b1a4390914136889af38a31f1b92eb7c0f1ca88c
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 16:44:59 -0300

    frontend: lendo os dados da API (ativos) colocando em uma tabela

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,17 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [user, setUser] = useState([]);
+  const [ativos, setAtivos] = useState([]);
   const [loading, setLoading] = useState(false);
 
   function getUser() {
-    fetch("http://localhost:3000/api/user/1")
+    fetch("http://localhost:3000/api/ativos")
           .then((res) => res.json())
           .then((data) => {
-            setUser(data);
+            setAtivos(data);
           })
           .catch((err) => {
-            console.error("Erro ao buscar usuários:", err);
+            console.error("Erro ao buscar ativos:", err);
           })
           .finally(() => {
             setLoading(false);
@@ -28,7 +28,24 @@ function App() {
     return (
     <div>
       <h1>App de investimentos!</h1>
-      <h2>Bem vindo! {user.nome }</h2>
+      <h2>Acompanhe seus ativos:</h2>
+      <table>
+        <th>Nome</th>
+        <th>Valor de compra</th>
+        <th>Qtd</th>
+        <th>Total</th>
+        <th>Deletar</th>
+          {ativos.map(atv => {
+            return (
+              <tr key={atv.id}>
+                <td>{atv.nome}</td>
+                <td>{atv.valor_compra}</td>
+                <td>{atv.quantidade}</td>
+                <td>{(atv.quantidade * atv.valor_compra).toFixed()}</td>
+                <td><button>Excluir</button></td>
+              </tr>
+            )})}
+      </table>
     </div>
   );
 }
