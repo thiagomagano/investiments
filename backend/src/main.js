87e8bc4e03da673481af2a269867d76c0292825f
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 14:41:53 -0300

    Conectando front e backend

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,10 +1,23 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   console.log("Hello World");
   return res.json({ msg: "Hello World!" });
+});
+app.get("/api/user/:id", (req, res) => {
+  const users = [
+    { "id": 1, "nome": "Thiago" },
+    { "id": 2, "nome": "João" }
+  ]
+  const id = parseInt(req.params.id);
+  const user = users.filter(usr => usr.id === id)[0];
+
+  return res.json(user);
 });
 
 app.listen(3000, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function getUser() {
+    fetch("http://localhost:3000/api/user/1")
+          .then((res) => res.json())
+          .then((data) => {
+            setUser(data);
+          })
+          .catch((err) => {
+            console.error("Erro ao buscar usuários:", err);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+  }
+
+  useEffect(() => {
+    getUser();
+  }, [])
+
+  if (loading) return <p>Carregando..</p>;
+
+    return (
     <div>
       <h1>App de investimentos!</h1>
+      <h2>Bem vindo! {user.nome }</h2>
     </div>
   );
 }
