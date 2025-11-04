91cfeaa12fbb0bb4d2e2d3a65a545a366e8835d0
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 17:39:03 -0300

    backend: adiconado rota de POST e DELETE para ativos

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ import 'dotenv/config'
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
 // Create the connection to database
@@ -25,6 +27,41 @@ app.get("/api/ativos", async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(404).json({status: 404, msg: "Investimentos não encontrados"})
+  }
+});
+
+app.post("/api/ativos", async (req, res) => {
+  const { nome, valor_compra, quantidade } = req.body
+
+
+    try {
+      const sql =
+        'INSERT INTO `ativos`(`nome`, `valor_compra`, `quantidade`) VALUES (?, ?, ?)';
+      const values = [nome, valor_compra, quantidade];
+
+      const [result, fields] = await connection.query(sql, values);
+
+      res.json(result);
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+app.delete("/api/ativos/:id", async (req, res) => {
+  const {id} = req.params
+
+  try {
+    const sql = 'DELETE FROM `ativos` WHERE `id` = ? LIMIT 1';
+    const values = [id];
+
+    const [result, fields] = await connection.execute(sql, values);
+
+    console.log(result);
+    console.log(fields);
+
+    res.json({status: "Deletado com sucesso", id: id})
+  } catch (err) {
+    res.status(500).json({msg: "Não foi possível deletar a linha"})
   }
 });
 
